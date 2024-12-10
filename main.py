f2986ef536995846af129a0f31b30c20b37b2e6d
WilliamsAl282 <WilliamsAl282@northwested.org>
2024-12-10 10:11:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-# Your name and your partner's name
-# Current date
+# Alex and Shay 
+# 12/10/2024
 # Validating String Input (Tiered Assignment)
 
 # Are you and your partner working on Level 1, Level 2 or Level 3 today?
-# Working on Level...
+# Working on Level 3
+
+
+input = input("Please enter a string: \n")
+
+if input.isalpha() == True and len(input) in range(6,11):
+    print(f"Valid input:{input} ")
+
+else:
+    print("Input must be alphabetic and have between 6 to 10 characters long")
+
 
